6f3c36440436d9bade9ce9708f1f8b9015135c0e
SurfaceBook\jamie <jamie.taylor@sheffield.ac.uk>
2020-04-23 21:29:53 +0100

    Fixed broken import of sp2dt

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version="0.1.1",
+    version="0.1.2",
 
     description="Convert between the settlement periods used by GB electricity industry and Unix timestamps.",
     long_description=long_description,
